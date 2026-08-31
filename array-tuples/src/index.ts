db79af1325ac45d86cb91de61f5d59117c9b0ece
aminul <aminulsohag10292@gmail.com>
2026-08-31 23:48:35 +0600

    array & tuples updated

--- array-tuples/src/index.ts
+++ array-tuples/src/index.ts
@@ -1,3 +1,15 @@
-import { greet } from "./utils.ts";
+// array and tuples
 
-console.log(greet("Md. Aminul Islam"));
+let scores: number[] = [85, 90, 78];
+let names: string[] = ["John", "Jane", "Bob"];
+
+let scores2: Array<number> = [50, 70, 60, 40];
+
+// tuple
+let student: [string, number, boolean] = ["John", 85, true];
+student = ["Aminul", 90, true];
+
+console.log(student);
+console.log(student[0]);
+console.log(student[1]);
+console.log(student.length);
