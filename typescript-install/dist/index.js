2656dc6cc1dd86b048f2d92282d30d88274efe03
aminul <aminulsohag10292@gmail.com>
2026-08-31 19:42:38 +0600

    installation typescript & configure completed

---
+++ typescript-install/dist/index.js
@@ -0,0 +1,4 @@
+import { greet } from "./utils.js";
+console.log(greet("Md. Aminul Islam"));
+console.log("Hi, Devs");
+//# sourceMappingURL=index.js.map
---
+++ typescript-install/src/index.ts
@@ -0,0 +1,4 @@
+import { greet } from "./utils.ts";
+
+console.log(greet("Md. Aminul Islam"));
+console.log("Hi, Devs");
