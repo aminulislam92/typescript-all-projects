fe5cbf61ce65e15141001f3ed8badc81c7ff8cff
aminul <aminulsohag10292@gmail.com>
2026-09-01 01:29:01 +0600

    any-unknown-void-null-undefine added

---
+++ any-unknown-void/src/index.ts
@@ -0,0 +1,44 @@
+// any, unknown, void, null, undefined
+
+// any type
+let randomValue: any = "Hello";
+
+randomValue = true;
+
+randomValue = 10;
+
+// unknown type
+let value: unknown = "World";
+
+if (typeof value === "string") {
+  console.log(value.toUpperCase());
+}
+
+// void type
+function logMessage(): void {
+  console.log("Logged successfully!");
+}
+
+logMessage();
+
+function greeting(name: string): string {
+  return `Hello ${name}`;
+}
+
+console.log(greeting("Md. Aminul Islam Sohag"));
+
+// undefined type
+let name;
+console.log(name);
+
+let age: undefined = undefined;
+console.log(age);
+
+age = 40;
+
+// null type
+let score = null;
+console.log(score);
+
+let nullValue: null = null;
+console.log(nullValue);
