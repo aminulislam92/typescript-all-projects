9094770ac817d0f08700751d2b89a330482d0205
aminul <aminulsohag10292@gmail.com>
2026-09-01 14:44:44 +0600

    arrow-function add

---
+++ arrow-function-13/src/index.ts
@@ -0,0 +1,13 @@
+// arrow function
+
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+
+const add2 = (a: number, b: number): number => a + b;
+
+const square = (x: number): number => x * x;
+
+console.log(add(55, 5));
+console.log(add2(30, 40));
+console.log(square(9));
