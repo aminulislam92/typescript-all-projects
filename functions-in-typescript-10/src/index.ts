78ff47216cafcbe2735c80af6c9a21b67dc6d0f6
aminul <aminulsohag10292@gmail.com>
2026-09-01 12:22:11 +0600

    function paramiter type, return type, function signature type

---
+++ functions-in-typescript-10/src/index.ts
@@ -0,0 +1,25 @@
+// functions in typescript
+// function functionName(){
+//     // logic
+// }
+
+// function with typescript
+
+// function functionName(parameterName: type, para2: type): returnType {
+//   // logic
+// }
+
+function greet(name: string): string {
+  return `Hello ${name}`;
+}
+
+console.log(greet("Snigdha"));
+
+// function type signature
+let greetFunction: (name: string) => string;
+
+greetFunction = function (name: string): string {
+  return `Hi ${name}`;
+};
+
+console.log(greetFunction("Lovely"));
